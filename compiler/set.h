4ba484da830d84c4be559f5802773c6bb9393b60
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2024-03-10 12:51:56 +0700

    refactor compiler: set

--- compiler/set.h
+++ compiler/set.h
@@ -1,11 +1,12 @@
 #ifndef SET_H
 #define SET_H
+#include <array>
 #include <vector>
 #include <string>
 
 namespace set {
     std::vector<char> compile(std::string input, int current_line);
-    std::vector<short> gen_instructions(short dest_instruction, short data);
+    std::vector<short> gen_instructions(short dest_instruction, short data, short address = 0);
 }
 
 #endif
