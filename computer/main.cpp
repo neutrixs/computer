87e99bfe24d9f6a9bd43cd1690bf9d44ecb247e3
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2024-03-12 20:14:20 +0700

    add command line argument support for computer

--- computer/main.cpp
+++ computer/main.cpp
@@ -3,13 +3,23 @@
 #include <vector>
 #include <fstream>
 #include <stdint.h>
+#include <string>
+#include <boost/filesystem.hpp>
 #include "loader.h"
 #include "components/processor.h"
-using namespace std;
 
-int main() {
-    string executable = read("executable");
-    vector<uint16_t> ROM = strtovec16(executable);
+int main(int argc, char** argv) {
+    std::string desc = "Usage: computer path/to/executable";
+    if (argc != 2) {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+
+    std::string input(argv[1]);
+    std::string input_abs = boost::filesystem::absolute(input).string();
+
+    std::string executable = read(input_abs);
+    std::vector<uint16_t> ROM = strtovec16(executable);
 
     Computer comp(ROM);
 
