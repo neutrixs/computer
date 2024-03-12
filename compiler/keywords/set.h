ae4f67a662cbdea1d9487d36c45480955b487cb3
Ihsan Rakhman <ihsan.maulana0501@gmail.com>
2024-03-12 22:56:36 +0700

    change the type of current_line to int from int32_t in set.h (compiler)

--- compiler/keywords/set.h
+++ compiler/keywords/set.h
@@ -6,7 +6,7 @@
 #include <stdint.h>
 
 namespace set {
-    std::vector<char> compile(std::string input, int32_t current_line);
+    std::vector<char> compile(std::string input, int current_line);
     std::vector<uint16_t> gen_instructions(uint16_t dest_instruction, uint16_t data, uint16_t address = 0);
 }
 
